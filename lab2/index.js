0210ca5042b5d86c0bf63236cb68776b65e74a86
Adrian Jeż <ajez@i2asolutions.com>
2016-10-13 23:58:34 +0200

    4, 5

--- lab2/index.js
+++ lab2/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var http = require("http");
+
 var app = express();
+
 
 /** Zad 2 **/
 app.use('/static', express.static(__dirname + '/public'));
@@ -35,32 +38,41 @@ app.get('/dodaj', function(req, res) {
 });
 
 
+/** Zad 4 **/
+// POST /dodaj2 gets JSON bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
+app.post('/dodaj2', function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+	daneGlowne.push(req.body.formData);
+	res.redirect('/dane');
+})
 
-app.get('/', function (req, res) {
-	res.send('Hello World');
+/** Zad 5 **/
+var osoby={};
+osoby['jnowak'] = {imie:'Janina', nazwisko: "Nowak", email:"jnowak@abc.com"};
+
+app.get('/osoby', function(req, res) {
+	res.write('<html><head></head><body>');
+  	res.write('<p>Liczba elementow tablicy osoby:'  + Object.keys(osoby).length + '</p>');
+  	res.write('<ul>');
+	for(var klucz in osoby){
+		res.write('<li>' + osoby[klucz].imie + 
+			' ' + osoby[klucz].nazwisko + 
+			' ' + osoby[klucz].email + '</li>');
+	}
+	res.end('</body></html>');
 });
 
-app.get('/time', function (req, res) {
-	res.send('Hello World TIME');
-});
-
-app.post('/time', function (req, res) {
-	res.status(418);
-	res.send('Teapot code');
-});
-
-app.put('/time', function (req, res) {
-	res.send('Hello World PUT');
-});
-
-app.delete('/time', function (req, res) {
-	res.send('Hello World DELETE');
-});
-
-var fun = function(a, b) {
-	console.log(Math.sqrt(Math.pow(a, 2)+ Math.pow(b, 2)));
-}
+/** ZAD 6 **/
+app.delete('/usun', function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+	daneGlowne.push(req.body.formData);
+	res.redirect('/dane');
+})
 
 
 var server = http.createServer(function (request, response) {
@@ -81,5 +93,5 @@ var server = http.createServer(function (request, response) {
 
 });
 
-app.listen(5006);
+app.listen(5001);
 console.log('Server działa na http://127.0.0.1:5000/');
